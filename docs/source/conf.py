100c86172962efbbd855fbf314262fb42093aa68
bmanjaree <manjaree@uw.edu>
2022-12-21 09:43:46 -0800

    added SWOT logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,4 +30,10 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-#html_static_path = ['_static']
+html_static_path = ['_static']
+html_static_path = ['_static']
+html_logo = "nasa_logo.png"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
